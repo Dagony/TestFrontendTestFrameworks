e678f855603550fd6cb3918d8ee5cd81c2ec8d87
Mark de Wit <dagony@gmail.com>
2018-06-16 20:48:20 +0200

    Extend test case

--- gemini/scriptinator.js
+++ gemini/scriptinator.js
@@ -1,10 +1,24 @@
 gemini.suite('scriptinator-login', (suite) => {
     suite.setUrl('/')
-        .setCaptureElements('#login-form')
-        .capture('plain')
-        .capture('with text', function(actions, find) {
+        .setCaptureElements(['#login-form'])
+        .before(function(actions, find) {
+            this.button = find('button');
+        })
+        .capture('01 - start', function(actions, find) {
+        })
+        .capture('02 - fill in username', function(actions, find) {
             actions.sendKeys(find('input[name="username"]'), 'mark');
+        })
+        .capture('03 - fill in password', function(actions, find) {
             actions.sendKeys(find('input[name="password"]'), 'Qe8du0vnpUKX');
-            actions.click(find('button'));
+        })
+        .capture('04 - button hovered', function(actions) {
+            actions.mouseMove(this.button)
+        })
+        .capture('05 - button pressed', function(actions) {
+            actions.mouseDown(this.button, 0);
+        })
+        .capture('06 - clicked', function(actions) {
+            actions.mouseUp(this.button);
         });
 });
